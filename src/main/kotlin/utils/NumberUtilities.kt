8765895384c30df77455574bcb675a6844648895
Your Name <youremail@example.com>
2026-04-25 00:55:20 +0100

    issue-12-add-validation-utilities

--- src/main/kotlin/utils/NumberUtilities.kt
+++ src/main/kotlin/utils/NumberUtilities.kt
@@ -1,2 +1,9 @@
-package ie.setu.utils
+package utils
 
+fun isValidRange(numberToCheck: Int, min: Int, max: Int): Boolean {
+    return numberToCheck in min..max
+}
+
+fun isValidDoubleRange(numberToCheck: Double, min: Double, max: Double): Boolean {
+    return numberToCheck in min..max
+}
